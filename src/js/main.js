a3df5f1cafa9e8a4d535af9192e756d27884800b
KoLLlaka <kotshredengera@gmail.com>
2022-06-02 15:49:53 +0300

    brands block and apadt

--- src/js/main.js
+++ src/js/main.js
@@ -119,5 +119,123 @@ function update(select, index) {
 	selectTitle.innerHTML = selectSelected.innerHTML;
 };
 
+// Swiper
+// BildSlider
+let sliders = document.querySelectorAll('._swiper');
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		if (!slider.classList.contains('swiper-bild')) {
+			let slider_items = slider.children;
+			if (slider_items) {
+				for (let index = 0; index < slider_items.length; index++) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+			let slider_content = slider.innerHTML;
+			let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');
+		}
+	}
+}
 
+if (document.querySelector('.slider-top')) {
+	let sliderBodyTop = document.querySelector('.slider-top').querySelector('.slider__body');
+	let itemSlider = new Swiper(sliderBodyTop, {
+		autoplay: {
+			delay: 2000,
+		},
+		observer: true,
+		observeParents: true,
+		slidesPerView: 5,
+		spaceBetween: 69,
+		speed: 1000,
+		loop: true,
+		// Dotts
+		pagination: {
+		},
+		// Arrows
+		navigation: {
+		},
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 20
+			},
+			// when window width is >= 480px
+			480: {
+				slidesPerView: 2,
+				spaceBetween: 30
+			},
+			// when window width is >= 640px
+			640: {
+				slidesPerView: 3,
+				spaceBetween: 20
+			},
+			970: {
+				slidesPerView: 4,
+				spaceBetween: 40
+			},
+			1200: {
+				slidesPerView: 5,
+				spaceBetween: 69
+			}
+		}
+	});
+}
 
+if (document.querySelector('.slider-bottom')) {
+	let sliderBodyBottom = document.querySelector('.slider-bottom').querySelector('.slider__body');
+	let itemSlider = new Swiper(sliderBodyBottom, {
+		autoplay: {
+			delay: 1500,
+		},
+		observer: true,
+		observeParents: true,
+		slidesPerView: 5,
+		spaceBetween: 65,
+		speed: 1500,
+		loop: true,
+		// Dotts
+		pagination: {
+		},
+		// Arrows
+		navigation: {
+		},
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 170
+			},
+			// when window width is >= 480px
+			480: {
+				slidesPerView: 1,
+				spaceBetween: 10
+			},
+			// when window width is >= 640px
+			640: {
+				slidesPerView: 2,
+				spaceBetween: 10
+			},
+			970: {
+				slidesPerView: 3,
+				spaceBetween: 30
+			},
+			1200: {
+				slidesPerView: 4,
+				spaceBetween: 40
+			},
+			1440: {
+				slidesPerView: 5,
+				spaceBetween: 65
+			}
+		}
+	});
+}
